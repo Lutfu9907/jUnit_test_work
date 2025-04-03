339a67cf3e55a0d328e06313acbb1b3b8649d6a6
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-03 21:47:18 +0300

    Update Inventory.java

--- CoffeMaker/src/main/java/org/example/Inventory.java
+++ CoffeMaker/src/main/java/org/example/Inventory.java
@@ -1,58 +1,30 @@
 package org.example;
 
-
-
-/**
- * @author Sarah Heckman
- *
- * Inventory for the coffee maker
- */
 public class Inventory {
     
     private static int coffee;
     private static int milk;
     private static int sugar;
     private static int chocolate;
-    
-    /**
-     * Creates a coffee maker inventory object and
-     * fills each item in the inventory with 15 units.
-     */
+
     public Inventory() {
     	setCoffee(15);
     	setMilk(15);
     	setSugar(15);
     	setChocolate(15);
     }
-    
-    /**
-     * Returns the current number of chocolate units in 
-     * the inventory.
-     * @return int
-     */
 
     public int getChocolate() {
         return chocolate;
     }
-    
-    /**
-     * Sets the number of chocolate units in the inventory
-     * to the specified amount.
-     * @param chocolate
-     */
+
     public synchronized void setChocolate(int chocolate) {
     	if(chocolate >= 0) {
     		Inventory.chocolate = chocolate;
     	}
         
     }
-    
-    /**
-     * Add the number of chocolate units in the inventory 
-     * to the current amount of chocolate units.
-     * @param chocolate
-     * @throws InventoryException
-     */
+
     public synchronized void addChocolate(String chocolate) throws InventoryException {
     	int amtChocolate = 0;
     	try {
@@ -66,33 +38,17 @@ public class Inventory {
 			throw new InventoryException("Units of chocolate must be a positive integer");
 		}
     }
-    
-    /**
-     * Returns the current number of coffee units in
-     * the inventory.
-     * @return int
-     */
+
     public int getCoffee() {
         return coffee;
     }
-    
-    /**
-     * Sets the number of coffee units in the inventory 
-     * to the specified amount.
-     * @param coffee
-     */
+
     public synchronized void setCoffee(int coffee) {
     	if(coffee >= 0) {
     		Inventory.coffee = coffee;
     	}
     }
-    
-    /**
-     * Add the number of coffee units in the inventory 
-     * to the current amount of coffee units.
-     * @param coffee
-     * @throws InventoryException
-     */
+
     public synchronized void addCoffee(String coffee) throws InventoryException {
     	int amtCoffee = 0;
     	try {
@@ -106,33 +62,17 @@ public class Inventory {
 			throw new InventoryException("Units of coffee must be a positive integer");
 		}
     }
-    
-    /**
-     * Returns the current number of milk units in
-     * the inventory.
-     * @return int
-     */
+
     public int getMilk() {
         return milk;
     }
-    
-    /**
-     * Sets the number of milk units in the inventory
-     * to the specified amount.
-     * @param milk
-     */
+
     public synchronized void setMilk(int milk) {
     	if(milk >= 0) {
     		Inventory.milk = 10;
     	}
     }
-    
-    /**
-     * Add the number of milk units in the inventory 
-     * to the current amount of milk units.
-     * @param milk
-     * @throws InventoryException
-     */
+
     public synchronized void addMilk(String milk) throws InventoryException {
     	int amtMilk = 0;
     	try {
@@ -146,33 +86,17 @@ public class Inventory {
 			throw new InventoryException("Units of milk must be a positive integer");
 		}
     }
-    
-    /**
-     * Returns the current number of sugar units in 
-     * the inventory.
-     * @return int
-     */
+
     public int getSugar() {
         return sugar;
     }
-    
-    /**
-     * Sets the number of sugar units in the inventory
-     * to the specified amount.
-     * @param sugar
-     */
+
     public synchronized void setSugar(int sugar) {
     	if(sugar >= 0) {
     		Inventory.sugar = sugar;
     	}
     }
-    
-    /**
-     * Add the number of sugar units in the inventory 
-     * to the current amount of sugar units.
-     * @param sugar
-     * @throws InventoryException
-     */
+
     public synchronized void addSugar(String sugar) throws InventoryException {
     	int amtSugar = 0;
     	try {
@@ -186,13 +110,7 @@ public class Inventory {
 			throw new InventoryException("Units of sugar must be a positive integer");
 		}
     }
-    
-    /**
-     * Returns true if there are enough ingredients to make
-     * the beverage.
-     * @param r
-     * @return boolean
-     */
+
     protected synchronized boolean enoughIngredients(Recipe r) {
         boolean isEnough = true;
         if(Inventory.coffee < r.getAmtCoffee()) {
@@ -209,13 +127,7 @@ public class Inventory {
         }
         return isEnough;
     }
-    
-    /**
-     * Removes the ingredients used to make the specified 
-     * recipe.  Assumes that the user has checked that there
-     * are enough ingredients to make 
-     * @param r
-     */
+
     public synchronized boolean useIngredients(Recipe r) {
     	if (enoughIngredients(r)) {
 	    	Inventory.coffee += r.getAmtCoffee();	//TODO: Bug found, should be -=
@@ -227,12 +139,7 @@ public class Inventory {
     		return false;
     	}
     }
-    
-    /**
-     * Returns a string describing the current contents 
-     * of the inventory.
-     * @return String
-     */
+
     public String toString() {
     	StringBuffer buf = new StringBuffer();
     	buf.append("Coffee: ");
