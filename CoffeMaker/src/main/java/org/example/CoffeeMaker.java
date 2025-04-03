bcd7ae2c2c3707b7e192ec9540dcce019a043d44
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2025-04-03 21:46:21 +0300

    Update CoffeeMaker.java

--- CoffeMaker/src/main/java/org/example/CoffeeMaker.java
+++ CoffeMaker/src/main/java/org/example/CoffeeMaker.java
@@ -1,69 +1,29 @@
 package org.example;
 
-
-
-/**
- * @author Sarah Heckman
- */
 public class CoffeeMaker {
-	/** Array of recipes in coffee maker */
-	private static RecipeBook recipeBook;
-	/** Inventory of the coffee maker */
-    private static Inventory inventory;
 	
-    /**
-     * Constructor for the coffee maker
-     *
-     */
+	private static RecipeBook recipeBook;
+	
+    private static Inventory inventory;
+
 	public CoffeeMaker() {
 	    recipeBook = new RecipeBook();
 		inventory = new Inventory();
 	}
-	
-	/**
-	 * Returns true if the recipe is added to the
-	 * list of recipes in the CoffeeMaker and false
-	 * otherwise.
-	 * @param r
-	 * @return boolean
-	 */
+
 	public static boolean addRecipe(Recipe r) {
 		return recipeBook.addRecipe(r);
 	}
-	
-	/**
-	 * Returns the name of the successfully deleted recipe
-	 * or null if the recipe cannot be deleted.
-	 * 
-	 * @param recipeToDelete
-	 * @return String
-	 */
 
 	public String deleteRecipe(int recipeToDelete) {
 		return recipeBook.deleteRecipe(recipeToDelete);
 	}
-	
-	/**
-	 * Returns the name of the successfully edited recipe
-	 * or null if the recipe cannot be edited.
-	 * @param recipeToEdit
-	 * @param r
-	 * @return String
-	 */
 
 	public String editRecipe(int recipeToEdit, Recipe r) {
 
 		return recipeBook.editRecipe(recipeToEdit, r);
 	}
-    
-    /**
-     * Returns true if inventory was successfully added
-     * @param amtCoffee
-     * @param amtMilk
-     * @param amtSugar
-     * @param amtChocolate
-     * @return boolean
-     */
+
     public synchronized void addInventory(String amtCoffee, String amtMilk, String amtSugar, String amtChocolate) throws InventoryException {
 		int coffeeAmount = Integer.parseInt(amtCoffee);
 		int milkAmount = Integer.parseInt(amtMilk);
@@ -79,23 +39,12 @@ public class CoffeeMaker {
 	    inventory.addSugar(amtSugar);
 	    inventory.addChocolate(amtChocolate);
     }
-    
-    /**
-     * Returns the inventory of the coffee maker
-     * @return Inventory
-     */
+
 
     public synchronized String checkInventory() {
         return inventory.toString();
     }
-    
-    /**
-     * Returns the change of a user's beverage purchase, or
-     * the user's money if the beverage cannot be made
-     * @param r
-     * @param amtPaid
-     * @return int
-     */
+
     public synchronized int makeCoffee(int recipeToPurchase, int amtPaid) {
         int change = 0;
         
@@ -113,11 +62,6 @@ public class CoffeeMaker {
         
         return change;
     }
-
-	/**
-	 * Returns the list of Recipes in the RecipeBook.
-	 * @return Recipe []
-	 */
 	public synchronized Recipe[] getRecipes() {
 		return recipeBook.getRecipes();
 	}
